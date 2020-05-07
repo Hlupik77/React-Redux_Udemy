137317a5cec3a3c8bbed17e19ef187f8ee4dbdfe
Hlupik77 <hlupik77@gmail.com>
2020-05-07 19:54:14 +0300

    state

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,30 +4,48 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
-  render (){
+  state = {
+    done: false
+  };
 
-const { label, important = false } = this.props;
+
+  onLabelClick = () => {
+    this.setState({
+      done: true
+    });
+  };
+
+
+  render() {
+    const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? 'steelblue' : 'black',
       fontWeight: important ? 'bold' : 'normal'
     };
-  
+
     return (
-      <span className="todo-list-item">
+      <span className={classNames}>
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={this.onLabelClick}>
           {label}
         </span>
-  
+
         <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
+          className="btn btn-outline-success btn-sm float-right">
           <i className="fa fa-exclamation" />
         </button>
-  
+
         <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
+          className="btn btn-outline-danger btn-sm float-right">
           <i className="fa fa-trash-o" />
         </button>
       </span>
