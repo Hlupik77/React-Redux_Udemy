8ad0134db6edb8124bba45141708e757ee40e0ed
Hlupik77 <hlupik77@gmail.com>
2020-05-12 00:29:56 +0300

    setState-редактирование

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -2,8 +2,7 @@ import React from 'react';
 import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
-
-const TodoList = ({ todos, onDeleted }) => {
+const TodoList = ({ todos, onDeleted, onToggleImportant, onToggleDone }) => {
 
   const elements = todos.map((item) => {
 
@@ -14,6 +13,10 @@ const TodoList = ({ todos, onDeleted }) => {
         <TodoListItem 
         {... itemProps} 
         onDeleted={() => onDeleted(id)}
+        onToggleImportant={()=> onToggleImportant(id)}
+        onToggleDone={()=> onToggleDone(id)}
+
+
         />
       </li>
     );
@@ -23,7 +26,7 @@ const TodoList = ({ todos, onDeleted }) => {
     <ul className="list-group todo-list">
       {elements}
     </ul>
-  );
+)
 };
 
 export default TodoList;
